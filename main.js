1a0625597ad7911b86c6d157c3c5c9fa3b0e367a
Mahi <ahnafraismahi@gmail.com>
2024-02-02 16:46:43 +0600

    testing

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#element', {
+        strings: ['App Developer....', 'Web Developer....'],
+        typeSpeed: 400,
+        loop:true,
+    });
+});
